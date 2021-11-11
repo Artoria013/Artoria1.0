4a5aa7b1baf9bed9d964e3e32d554ad677f48b55
Haixin Li <2388559709@qq.com>
2021-11-11 18:15:41 +0800

    clean

--- test2.java
+++
@@ -1,9 +0,0 @@
-public class TEST2 {
-    public static void main(String[]args){
-    int d=3;
-    double s=3.14*d*d;
-    double c=6.28*d;
-    System.out.println("该圆的周长为:"+c);
-    System.out.print("该圆的面积为:"+s);
-    }
-}
